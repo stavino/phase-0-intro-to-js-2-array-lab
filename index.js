7cfe93d078275b1703cf0cbbf0c04dc4c7d7b039
Stephen Avino <stephen.avino@gmail.com>
2021-10-08 00:12:07 -0400

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// Write your solution here!
+const cats = [
+ "Milo",
+ "Otis",
+ "Garfield",   
+];
+function destructivelyAppendCat(name){
+    return cats.push(`${name}`)
+}
+function destructivelyPrependCat(name){
+    return cats.unshift(`${name}`)
+};
+function destructivelyRemoveLastCat(){
+    return cats.pop()
+};
+function destructivelyRemoveFirstCat(){
+    return cats.shift()
+};
+let catCopy;
+function appendCat(name){
+    return catCopy = [...cats,`${name}`]
+};
+function prependCat(name){
+    return catCopy = [`${name}`, ...cats]
+};
+function removeLastCat(){
+    return catCopy = [...cats.slice(0,2)]
+};
+function removeFirstCat(){
+    return catCopy = [...cats.slice(1,3)]
+};
